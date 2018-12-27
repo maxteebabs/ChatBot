ce19ae75259e989227bce0dbe7a0cab48223f2b7
Taiwo Famurewa <famurewa_taiwo@yahoo.com>
2018-12-27 18:46:55 +0100

    changes to bot app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FlightsComponent } from './flights/flights.component';
 import { BotComponent } from './bot/bot.component';
 // import { BotModule } from './bot/bot.module';
 
@@ -12,7 +11,6 @@ import{BotService} from './bot.service';
 @NgModule({
   declarations: [
     AppComponent,
-    FlightsComponent,
     BotComponent
   ],
   imports: [
